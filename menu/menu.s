75747596cbd3c14b8070cb4a0c51d35f12c763a7
lucixsg <alu.151560@usj.es>
2024-11-14 18:28:32 +0100

    Update menu.s

--- menu/menu.s
+++ menu/menu.s
@@ -18,11 +18,27 @@ main:
 loop:
 #imprimir texto del menu
     li $v0 4
-    la $a0 menu
+    la $a0 menu # imprimir menú
     syscall
 
-    li $v0 12
+    li $v0 12 # leer caracter
+    lb $t0 $v0 # se guarda el carácter en t0
+
+    # según la entrada del usuario, se llama a la función
+    beq $t0, "H", leerHex
+    beq $t0, "N", leerRGB
+    beq $t0, "I", consulta
+    beq $t0, "R", filtroR
+    beq $t0, "V", filtroV
+    beq $t0, "A", filtroA
+    beq $t0, "Y", filtroY
+    beq $t0, "C", filtroC
+    beq $t0, "M", filtroM
+
+    bne $v0, "S", loop # si el character no es S
+    syscall
+    li $v0 10 # salir del programa
+    syscall
 
 
-    bne $v0, "S", loop
     
