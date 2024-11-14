f537315159d7654734b3889d2b0ec97930f78c94
Eduardo Villar <alu.142886@usj.es>
2024-11-14 11:48:56 +0100

    avanzando en ingresarHex

--- IngresarValores/ingresarHexadecimal.s
+++ IngresarValores/ingresarHexadecimal.s
@@ -5,28 +5,103 @@ actualGreen: .byte 0
 actualRed: .byte 0
 .byte 0 
 
+str_buffer: .space 6
+str_fin: .byte 0
+
+str_error: .asciiz "ERROR"
+
 .text
 .globl main
 
 main:
 
-    li $v0 8    #llamada a leer string
-    la $a0 $t0  #string se guarda en t0
+    li $v0 8                #llamada a leer string
+    la $a0 str_buffer       #indicamos la direccion de memoria donde se guardara el str 
+    li $a1 6                #indicamos la longitud maxima del string
     syscall
 
-    li $t1 0    #iniciamos t0 a 0, sera donde se guardaran los valores de color
-    li $t2 0    #iniciamos t1 a 0, nos servira para ir iterando sobre el string
-    la $t3 $t0    #iniciamos t2 a 0, sera donde guardaremos la direccion de memoria a la que queremos acceder en cada bucle
+
+    li $a0 0                #iniciamos a0 a 0, sera el valor del byte actual de color 
+    li $a1 0                #iniciamos a1 a 0, sera donde se guardaran el valor total del color
+    la $a2 str_buffer       #cargamos la direccion de memoria del str a a2
 
 while1:
 
-    la $t3 ($t0)
+    move $a0 $a2              #guardamos el valor del caracter correspondiente en a0
+    beqz $a0 fin1           #si el caracter actual es 0, el bucle termina
 
-    bnez 
+    mul $a1 $a1 16        
+    
+    #llamamos a la funcion h2i para calcular el valor int del byte
+    jal h2i
+    
+    add $a1 $a1 $v0
 
+    addu $a2 1              #Sumamos 1 a la direccion de memoria para ir al siguiente byte
+
+    j while1                #Repetimos bucle
 
 fin1:
 
+    sw $a1 actualColor      #Guardamos el resultado almacenado en $a1 en la direccion del color
+
+    #IMRPIMIR POR PANTALLA: QUITAR
+    li $v0 4
+    la $a0 actualColor
+    syscall
+
+    li $v0 10
+    syscall
+
+h2i:
+    
+    # #prologo
+    # subu $sp $sp 24
+    # sw $a0 20($sp) 
+    # sw $ra 4($sp)
+    # sw $s0 ($sp)
+
+    #prologo
+    subu $sp $sp 8
+    sw $a0 4($sp)
+
+    blt $a0 '0'else_if      #Si el char actual es menor que '0' no cumple los parametros y va al else_if
+    bgt $t0 '9' else_if	    #Si el char actual es mayor que '9' no cumple los parametros y va al else_if
+    
+    # 0 <= char <= 9
+
+    sub $v0 $a0 '0'
+
+else_if:
+
+    blt $a0 'A' error       #Si el char actual es menor que 'A' no cumple los parametros y va al error
+    bgt $t0 'F' error	    #Si el char actual es mayor que 'F' no cumple los parametros y va al error
+
+    # 'A' <= char <= 'F'
+
+    sub $v0 $a0 '9'
+
+error:
+
+    li $v0 4
+    la $a0 str_error
+    syscall
+
+    li $v0 10
+    syscall
 
 
+finh2i:
+
+    # #epilogo
+    # lw $a0 20($sp)
+    # lw $ra 4($sp)
+    # lw $s0 ($sp)
+    # addu $sp $sp 24
+
+    #prologo
+    lw $a0 4($sp)
+    addu $sp $sp 8
+
+    jr $ra
 
