a9ff70744bf93ddbdfc5c7e447ae251200c828b4
Eduardo Villar <alu.142886@usj.es>
2024-12-02 12:07:28 +0100

    fixed read hex

--- IngresarValores/ingresarHexadecimal.s
+++ IngresarValores/ingresarHexadecimal.s
@@ -5,15 +5,23 @@ actualGreen: .byte 0
 actualRed: .byte 0
 .byte 0 
 
-str_buffer: .space 6
-str_fin: .byte 0
+# str_buffer: .space 6
+# str_fin: .byte 0
 
 str_error: .asciiz "ERROR"
+str_read_hex: .asciiz "INGRESE VALOR HEXADECIMAL: "
+
+str_buffer: .space 6
+str_fin: .word 0
 
 .text
 .globl main
 
 main:
+
+    li $v0, 4               #mensaje pidiendo string
+    la $a0, str_read_hex
+    syscall
 
     li $v0 8                #llamada a leer string
     la $a0 str_buffer       #indicamos la direccion de memoria donde se guardara el str 
@@ -28,7 +36,8 @@ main:
 while1:
 
     lb $a0 ($a2)            #guardamos el valor del caracter correspondiente en a0
-    beqz $a0 fin1           #si el caracter actual es 0, el bucle termina
+    # beqz $a0 fin1           #si el caracter actual es 0, el bucle termina
+    beq $a0 10 fin1         #si el caracter actual es 10 (LINE FEED), el bucle termina
 
     mul $a1 $a1 16        
     
@@ -45,11 +54,6 @@ fin1:
 
     sw $a1 actualColor      #Guardamos el resultado almacenado en $a1 en la direccion del color
 
-    #IMRPIMIR POR PANTALLA: QUITAR
-    li $v0 4
-    lw $a0 actualColor
-    syscall
-
     li $v0 10
     syscall
 
@@ -65,8 +69,8 @@ h2i:
     subu $sp $sp 8
     sw $a0 4($sp)
 
-    blt $a0 '0' else_if      #Si el char actual es menor que '0' no cumple los parametros y va al else_if
-    bgt $t0 '9' else_if	    #Si el char actual es mayor que '9' no cumple los parametros y va al else_if
+    blt $a0 '0' h2i_elif      #Si el char actual es menor que '0' no cumple los parametros y va al h2i_elif
+    bgt $a0 '9' h2i_elif	  #Si el char actual es mayor que '9' no cumple los parametros y va al h2i_elif
     
     # 0 <= char <= 9
 
@@ -74,18 +78,18 @@ h2i:
 
     j finh2i
 
-else_if:
+h2i_elif:
 
-    blt $a0 'A' error       #Si el char actual es menor que 'A' no cumple los parametros y va al error
-    bgt $t0 'F' error	    #Si el char actual es mayor que 'F' no cumple los parametros y va al error
+    blt $a0 'A' h2i_error       #Si el char actual es menor que 'A' no cumple los parametros y va al h2i_error
+    bgt $a0 'F' h2i_error	    #Si el char actual es mayor que 'F' no cumple los parametros y va al h2i_error
 
     # 'A' <= char <= 'F'
 
-    sub $v0 $a0 '9'
+    sub $v0 $a0 '7'
 
     j finh2i
 
-error:
+h2i_error:
 
     li $v0 4
     la $a0 str_error
